8a78d47d22b2e2043c25e91c0bbdb9ec5625c8d4
shuai <shuai@git.com>
2023-01-04 14:10:56 +0800

    first commit

---
+++ src/main/java/com/shuai/GitTest.java
@@ -0,0 +1,12 @@
+package com.shuai;
+
+/**
+ * @Author: fengxin
+ * @CreateTime: 2023-01-04  14:06
+ * @Description: TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
